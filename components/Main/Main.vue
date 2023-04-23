2f3427b297e53af81501c1a8019f29ecb789ee92
JCaeta <63880187+JCaeta@users.noreply.github.com>
2023-04-23 14:03:43 -0300

    First commit

---
+++ components/Main/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- Your component's HTML goes here -->
+    <h1>Main component</h1>
+    <p>Put your HTML elements here</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {
+      // Your component's data goes here
+    }
+  },
+  methods: {
+    // Your component's methods go here
+  }
+  // Other component options like computed properties and lifecycle hooks can also go here
+}
+</script>
+
+<style>
+/* Your component's CSS goes here */
+</style>
